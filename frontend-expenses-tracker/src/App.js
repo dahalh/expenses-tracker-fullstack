9909833c92c00e39a1e7bf40f98c089f36fbe344
dahalh <dahalh@outlook.com>
2022-05-10 09:04:03 +1000

    frontend cleanup

---
+++ frontend-expenses-tracker/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello World
+      <Button variant="primary">
+        Primary <i class="fa-solid fa-0"></i>
+      </Button>
+    </div>
+  );
+}
+
+export default App;
