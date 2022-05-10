7eacfb8089f8056c532d8618a3fbc9156a14d958
dahalh <dahalh@outlook.com>
2022-05-10 14:36:24 +1000

    login feature done

--- frontend-expenses-tracker/src/helpers/axiosHelper.js
+++ frontend-expenses-tracker/src/helpers/axiosHelper.js
@@ -2,10 +2,25 @@ import axios from "axios";
 
 const rootUrl = "http://localhost:8000/api/v1";
 const userApi = rootUrl + "/users";
+const loginApi = rootUrl + "/users/login";
 
-export const postRegister = async (formData) => {
+export const postRegister = (formData) => {
   try {
     return axios.post(userApi, formData);
+  } catch (error) {
+    const data = {
+      status: "error",
+      message: error.message,
+    };
+    return {
+      data,
+    };
+  }
+};
+
+export const postLogin = (formData) => {
+  try {
+    return axios.post(loginApi, formData);
   } catch (error) {
     const data = {
       status: "error",
